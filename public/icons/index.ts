52fef538edbe483033e42c98e34d5cb81a48ee96
Devinix00 <dpldpl123456@naver.com>
2024-05-04 16:40:32 +0900

    feat: 헤더 검색 드롭다운 퍼블리싱

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,3 +1,4 @@
+export { default as Fire } from "./fire.svg";
 export { default as HeaderUserArrow } from "./header-user-arrow.svg";
 export { default as KakaoLogo } from "./kakao-logo.svg";
 export { default as NaverLogo } from "./naver-logo.svg";
--- src/components/header/headerRightSection/HeaderSearchInput.tsx
+++ src/components/header/headerRightSection/HeaderSearchInput.tsx
@@ -1,11 +1,11 @@
-import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 
 import ROUTES from "@/constants/routes";
 
-import { Fire, Search, WhiteSearch } from "../../../../public/icons";
+import { Search, WhiteSearch } from "../../../../public/icons";
+import HeaderSearchDropdown from "./HeaderSearchDropdown";
 
 interface HeaderSearchInputProps {
   hasScrolledPast: boolean;
@@ -16,31 +16,44 @@ export default function HeaderSearchInput({
 }: HeaderSearchInputProps) {
   const pathname = usePathname();
   const [inputFocused, setInputFocused] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <div className="relative w-[360px]">
       {pathname === ROUTES.DETAIL ? (
         hasScrolledPast ? (
           <input
+            value={inputValue}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setInputValue(e.target.value)
+            }
             placeholder="‘파묘’ 궁금하지 않으세요?"
             onFocus={() => setInputFocused(true)}
             onBlur={() => setInputFocused(false)}
-            className={`flex h-10 w-full items-start rounded-[20px] border-D2_Gray bg-[#2e2c2b] pl-[64px] pr-[24px] font-Medium text-L_Gray outline-none placeholder:text-L_Gray placeholder:transition-all placeholder:duration-200 hover:border focus:placeholder:opacity-0 ${inputFocused && "bg-D2_Gray"} transition-all duration-200`}
+            className={`flex h-10 w-full items-start border-D2_Gray bg-[#2e2c2b] pl-[64px] pr-[24px] font-Medium text-L_Gray outline-none placeholder:text-L_Gray hover:border focus:placeholder:opacity-0 ${inputFocused ? "rounded-t-[20px] bg-D2_Gray" : "rounded-[20px]"}`}
           />
         ) : (
           <input
+            value={inputValue}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setInputValue(e.target.value)
+            }
             placeholder="‘파묘’ 궁금하지 않으세요?"
             onFocus={() => setInputFocused(true)}
             onBlur={() => setInputFocused(false)}
-            className={`flex h-10 w-full items-start rounded-[20px] bg-transparent pl-[64px] pr-[24px] font-Medium text-[rgba(255,255,255,0.6)] outline-none placeholder:text-[rgba(255,255,255,0.6)] placeholder:transition-opacity placeholder:duration-200 hover:border-Silver focus:placeholder:opacity-0 ${inputFocused ? "bg-D2_Gray" : "border border-[rgba(255,255,255,0.6)]"} transition-all duration-200`}
+            className={`flex h-10 w-full items-start bg-transparent pl-[64px] pr-[24px] font-Medium text-[rgba(255,255,255,0.6)] outline-none placeholder:text-[rgba(255,255,255,0.6)] hover:border-Silver focus:placeholder:opacity-0 ${inputFocused ? "rounded-t-[20px] bg-D2_Gray" : "rounded-[20px] border border-[rgba(255,255,255,0.6)]"}`}
           />
         )
       ) : (
         <input
+          value={inputValue}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setInputValue(e.target.value)
+          }
           placeholder="‘파묘’ 궁금하지 않으세요?"
           onFocus={() => setInputFocused(true)}
           onBlur={() => setInputFocused(false)}
-          className={`flex h-10 w-full items-start rounded-[20px] border-D2_Gray bg-[#2e2c2b] pl-[64px] pr-[24px] font-Medium text-L_Gray outline-none placeholder:text-L_Gray placeholder:transition-all placeholder:duration-200 hover:border focus:placeholder:opacity-0 ${inputFocused && "bg-D2_Gray"} transition-all duration-200`}
+          className={`flex h-10 w-full items-start border-D2_Gray bg-[#2e2c2b] pl-[64px] pr-[24px] font-Medium text-L_Gray outline-none placeholder:text-L_Gray hover:border focus:placeholder:opacity-0 ${inputFocused ? "rounded-t-[20px] bg-D2_Gray" : "rounded-[20px]"}`}
         />
       )}
       <Image
@@ -54,6 +67,7 @@ export default function HeaderSearchInput({
         alt=""
         className="absolute left-[24px] top-[50%] translate-y-[-50%]"
       />
+      {inputFocused && <HeaderSearchDropdown inputValue={inputValue} />}
     </div>
   );
 }
