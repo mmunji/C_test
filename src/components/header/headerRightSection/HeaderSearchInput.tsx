eb44b554eba63da634f3d356787404882336caa9
Devinix00 <dpldpl123456@naver.com>
2024-05-04 15:47:49 +0900

    feat: 헤더 검색창 포커스 애니메이션 추가

--- src/components/header/headerRightSection/HeaderSearchInput.tsx
+++ src/components/header/headerRightSection/HeaderSearchInput.tsx
@@ -1,10 +1,11 @@
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 import ROUTES from "@/constants/routes";
 
-import { Search, WhiteSearch } from "../../../../public/icons";
+import { Fire, Search, WhiteSearch } from "../../../../public/icons";
 
 interface HeaderSearchInputProps {
   hasScrolledPast: boolean;
@@ -14,24 +15,32 @@ export default function HeaderSearchInput({
   hasScrolledPast,
 }: HeaderSearchInputProps) {
   const pathname = usePathname();
+  const [inputFocused, setInputFocused] = useState(false);
+
   return (
     <div className="relative w-[360px]">
       {pathname === ROUTES.DETAIL ? (
         hasScrolledPast ? (
           <input
             placeholder="‘파묘’ 궁금하지 않으세요?"
-            className="flex h-10 w-full items-start rounded-[20px] border-D2_Gray bg-[#2e2c2b] pl-[64px] pr-[24px] font-Medium text-L_Gray outline-none placeholder:text-L_Gray hover:border"
+            onFocus={() => setInputFocused(true)}
+            onBlur={() => setInputFocused(false)}
+            className={`flex h-10 w-full items-start rounded-[20px] border-D2_Gray bg-[#2e2c2b] pl-[64px] pr-[24px] font-Medium text-L_Gray outline-none placeholder:text-L_Gray placeholder:transition-all placeholder:duration-200 hover:border focus:placeholder:opacity-0 ${inputFocused && "bg-D2_Gray"} transition-all duration-200`}
           />
         ) : (
           <input
             placeholder="‘파묘’ 궁금하지 않으세요?"
-            className="flex h-10 w-full items-start rounded-[20px] border border-[rgba(255,255,255,0.6)] bg-transparent pl-[64px] pr-[24px] font-Medium text-[rgba(255,255,255,0.6)] outline-none placeholder:text-[rgba(255,255,255,0.6)] hover:border-Silver"
+            onFocus={() => setInputFocused(true)}
+            onBlur={() => setInputFocused(false)}
+            className={`flex h-10 w-full items-start rounded-[20px] bg-transparent pl-[64px] pr-[24px] font-Medium text-[rgba(255,255,255,0.6)] outline-none placeholder:text-[rgba(255,255,255,0.6)] placeholder:transition-opacity placeholder:duration-200 hover:border-Silver focus:placeholder:opacity-0 ${inputFocused ? "bg-D2_Gray" : "border border-[rgba(255,255,255,0.6)]"} transition-all duration-200`}
           />
         )
       ) : (
         <input
           placeholder="‘파묘’ 궁금하지 않으세요?"
-          className="flex h-10 w-full items-start rounded-[20px] border-D2_Gray bg-[#2e2c2b] pl-[64px] pr-[24px] font-Medium text-L_Gray outline-none placeholder:text-L_Gray hover:border"
+          onFocus={() => setInputFocused(true)}
+          onBlur={() => setInputFocused(false)}
+          className={`flex h-10 w-full items-start rounded-[20px] border-D2_Gray bg-[#2e2c2b] pl-[64px] pr-[24px] font-Medium text-L_Gray outline-none placeholder:text-L_Gray placeholder:transition-all placeholder:duration-200 hover:border focus:placeholder:opacity-0 ${inputFocused && "bg-D2_Gray"} transition-all duration-200`}
         />
       )}
       <Image
