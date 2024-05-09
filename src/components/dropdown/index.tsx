4c1c85ce4064ae4677926e1e3b0f4a7c8558758b
bisari31 <bisari31@gmail.com>
2024-05-10 02:31:12 +0900

    CIN-27: 주석 설명 추가

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -1,18 +1,43 @@
 import React from "react";
 
+/** 사용 예시
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const ref = useOutsideClick(() => setIsDropdownOpen(false));
+
+  return (
+        <div className="relative" ref={ref}>
+            < 버튼 >
+          <Dropdown
+            onClose={() => setIsDropdownOpen(false)}
+            isOpen={isDropdownOpen}
+            className=""
+          >
+            <Dropdown.Item onClick={() => console.log("수정")} isFocused>
+              수정
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => console.log("삭제")}>
+              삭제
+            </Dropdown.Item>
+            <Dropdown.Item onClick={() => console.log("삭제")}>
+              삭제2
+            </Dropdown.Item>
+          </Dropdown>
+        </div>
+ */
+
 interface DropdownItemProps {
   children: React.ReactNode;
-  onClick: () => void;
-  isGenre?: boolean;
-  isFocused?: boolean;
-  hasIcon?: boolean;
+  onClick: () => void; // 클릭 이벤트를 넘겨주세요.
+  isGenre?: boolean; // 장르 드롭다운 사용 시 true로 설정해주세요.
+  isFocused?: boolean; // 포커스된 아이템에게 true로 설정해주세요.
+  hasIcon?: boolean; // svg를 사용하는 드롭다운의 경우 true로 설정해주세요.  (< hasIcon || isGenre || 기본 > 값으로 패딩이 적용됩니다.)
 }
 interface DropdownMainProps {
   children: React.ReactNode;
   isOpen: boolean;
-  onClose: () => void;
-  isGenre?: boolean;
-  className?: string;
+  onClose: () => void; // 아이템 클릭 이벤트가 작동 후에 콜백으로 onClose 함수가 실행됩니다.
+  isGenre?: boolean; // 장르 드롭다운 사용 시 true로 설정해주세요.
+  className?: string; // "bottom-5 left-5" 등 추가하여 디테일한 위치를 설정할 수 있습니다.
 }
 
 function DropdownItem({
