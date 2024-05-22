479ff18ad9999a3d9da452fc636570e3dc4fe68b
Devinix00 <dpldpl123456@naver.com>
2024-05-22 23:12:10 +0900

    CIN-25: 한글 입력수 제한 버그 해결

--- src/app/detail/_components/keywordAndTalkAndGallery/keyword/keywordForm.tsx
+++ src/app/detail/_components/keywordAndTalkAndGallery/keyword/keywordForm.tsx
@@ -1,31 +1,21 @@
-import { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { ChangeEvent, useState } from "react";
 
 import KeywordSpeechBubble from "./KeywordSpeechBubble";
 
 export default function KeywordForm() {
   const [focused, setFocused] = useState(false);
-  const { register, handleSubmit, watch, setValue } = useForm<{
-    value: string;
-  }>();
+  const [value, setValue] = useState("");
 
-  const onSubmit: SubmitHandler<{ value: string }> = (value) => {
-    try {
-      console.log(value);
-    } catch (error) {
-      alert(error);
-    } finally {
-      setValue("value", "");
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length > 5) {
+      e.target.value = e.target.value.slice(0, 5);
+    } else {
+      setValue(e.target.value);
     }
   };
 
-  const value = watch("value");
-
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="relative w-full Laptop:static"
-    >
+    <form className="relative w-full Laptop:static">
       <KeywordSpeechBubble />
       <div className="relative w-full overflow-hidden rounded-xl ">
         <input
@@ -33,10 +23,7 @@ export default function KeywordForm() {
           placeholder="‘웡카’를 한단어로 말한다면?"
           maxLength={5}
           onFocus={() => setFocused(true)}
-          {...register("value", {
-            required: true,
-            onBlur: () => setFocused(false),
-          })}
+          onChange={(e) => handleChange(e)}
           className="h-[45px] w-full bg-[rgba(0,0,0,0.20)] py-2 pl-4 pr-3 text-Gray_Orange outline-none Text-s-Medium placeholder:text-Gray Tablet:Text-m-Medium"
         />
 
