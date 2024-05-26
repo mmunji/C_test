39cb611d807ddbc8c1e8010e19e67aacc9703d15
bisari31 <bisari31@gmail.com>
2024-05-26 17:29:21 +0900

    feat: routes path 변경(my)

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,12 @@
 const ROUTES = {
   MAIN: "/",
   DETAIL: "/detail",
-  MY: "/my",
+  MY: {
+    default: "/my",
+    activity: () => `${ROUTES.MY.default}/activity`,
+    account: () => `${ROUTES.MY.default}/account`,
+    favorites: () => `${ROUTES.MY.default}/favorites`,
+  },
 };
 
 export default ROUTES;
