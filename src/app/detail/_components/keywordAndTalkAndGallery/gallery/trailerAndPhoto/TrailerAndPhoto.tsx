3e03c9707344e2f1862a69c965ed84d0d0845446
Devinix00 <dpldpl123456@naver.com>
2024-05-26 17:34:10 +0900

    CIN-25: DetailActiveTab 애니메이션 버그 수정

--- src/app/detail/_components/keywordAndTalkAndGallery/gallery/trailerAndPhoto/TrailerAndPhoto.tsx
+++ src/app/detail/_components/keywordAndTalkAndGallery/gallery/trailerAndPhoto/TrailerAndPhoto.tsx
@@ -2,8 +2,8 @@
 
 import React, { useState } from "react";
 
+import DetailActiveTab from "../../../common/DetailActiveTab";
 import TrailerAndPhotoSlider from "../../../common/TraillerAndPhotoSlider";
-import DetailActiveTab from "../../../DetailActiveTab";
 
 export default function TrailerAndPhoto() {
   const tabs = ["예고편", "포토"];
@@ -11,7 +11,11 @@ export default function TrailerAndPhoto() {
 
   return (
     <div>
-      <DetailActiveTab {...{ tabs, activeTab, setActiveTab }} />
+      <DetailActiveTab {...{ tabs, activeTab, setActiveTab }}>
+        <div
+          className={`absolute bottom-[3px] h-0.5 w-5 bg-Primary transition-all duration-300 ease-out ${activeTab === tabs[0] ? "translate-x-[22.5px] Laptop:translate-x-[27.75px]" : "translate-x-[82px] Laptop:translate-x-[95px]"}`}
+        />
+      </DetailActiveTab>
       <TrailerAndPhotoSlider />
     </div>
   );
