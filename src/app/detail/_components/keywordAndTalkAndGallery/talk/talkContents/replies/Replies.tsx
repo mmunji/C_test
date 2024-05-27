680335b9471bce5130029513f2135c95ae74fa2d
Devinix00 <dpldpl123456@naver.com>
2024-05-28 00:04:18 +0900

    CIN25: ReplyForm 퍼블리싱

---
+++ src/app/detail/_components/keywordAndTalkAndGallery/talk/talkContents/replies/Replies.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import ReplyForm from "./ReplyForm";
+
+export default function Replies() {
+  return (
+    <div className="ml-9 Tablet:ml-14 Laptop:ml-[52px]">
+      <ReplyForm />
+    </div>
+  );
+}
