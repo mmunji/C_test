4f15e66de32f44a09dc892c5638b8b91832f33ec
Devinix00 <dpldpl123456@naver.com>
2024-05-28 22:17:52 +0900

    CIN-25: KeywordBar 로딩 스피너 처리

--- src/app/detail/_components/common/DetailBadge.tsx
+++ src/app/detail/_components/common/DetailBadge.tsx
@@ -8,6 +8,7 @@ interface DetailBadgeProps {
 
 export default function DetailBadge({ content }: DetailBadgeProps) {
   const backgroundColor = hexToRGBA("#000000", 0.2);
+
   return (
     <p
       className="flex h-full items-center justify-center rounded-lg px-2 text-Silver Text-xs-Regular Tablet:Text-s-Medium"
