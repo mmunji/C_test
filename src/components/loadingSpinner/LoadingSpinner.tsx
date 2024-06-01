2f25750d702a5cd9e58819b94f90fd93d3a81820
Devinix00 <dpldpl123456@naver.com>
2024-06-01 22:46:54 +0900

    Reapply "CIN-25: 헤더 검색창 hover시에도 사이즈 고정"
    This reverts commit 884f09d26169360a67025d636756be87f8ec40de.

--- src/components/loadingSpinner/LoadingSpinner.tsx
+++ src/components/loadingSpinner/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 
 interface LoadingSpinnerProps {
-  size: "sm" | "md" | "lg";
+  size: "xs" | "sm" | "md" | "lg";
   color: "primary" | "white";
 }
 
@@ -9,10 +9,11 @@ export default function LoadingSpinner({ size, color }: LoadingSpinnerProps) {
   return (
     <div
       className={clsx(
-        "animate-spin rounded-full border-4 border-t-BG",
-        { "h-6 w-6": size === "sm" },
-        { "h-9 w-9": size === "md" },
-        { "h-12 w-12": size === "lg" },
+        "animate-spin rounded-full border-2 border-t-transparent",
+        { "h-4 w-4 border-2": size === "xs" },
+        { "h-6 w-6 border-4": size === "sm" },
+        { "h-9 w-9 border-4": size === "md" },
+        { "h-12 w-12 border-4": size === "lg" },
         { "border-Primary": color === "primary" },
         { "border-White": color === "white" },
       )}
