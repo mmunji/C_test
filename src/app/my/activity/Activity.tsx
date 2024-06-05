b0f384857feb2ee2920f3f3289302bcf0e945a53
bisari31 <bisari31@gmail.com>
2024-06-06 01:19:25 +0900

    CIN-32: Activity tabs 공통 컴포넌트로 변경

--- src/app/my/activity/Activity.tsx
+++ src/app/my/activity/Activity.tsx
@@ -7,19 +7,18 @@ import { useState } from "react";
 import RatingList from "@/app/my/activity/RatingList";
 import ReviewList from "@/app/my/activity/ReviewList";
 import Button from "@/components/buttons/Button";
+import CommonTab from "@/components/commonTab/CommonTab";
 import Dropdown from "@/components/dropdown/Dropdown";
 import ROUTES from "@/constants/routes";
 
 import { Filter } from "../../../../public/icons";
 
-const TAB_MENU = ["톡", "평가로그"];
-
 export default function Activity() {
+  const tabs = ["톡 5551", "평가로그 123"];
   const [filter, setFilter] = useState<"최신순" | "좋아요순">("최신순");
-  const [selectedTab, setSelectedTab] = useState(0);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
   const pathname = usePathname();
   const isActivityPage = pathname === ROUTES.MY.activity();
-
   return (
     <section
       className={`${isActivityPage ? "flex" : "hidden"} flex-col gap-3 px-5 Tablet:flex Tablet:gap-4 Tablet:px-0`}
@@ -28,21 +27,13 @@ export default function Activity() {
         내 활동
       </h2>
       <div className="flex items-center justify-between">
-        <div className="flex items-center">
-          {TAB_MENU.map((menu, i) => (
-            <button
-              key={menu}
-              onClick={() => setSelectedTab(i)}
-              type="button"
-              className={`${selectedTab === i ? "Text-m-Bold Tablet:Text-l-Bold" : "text-Gray Text-m-Medium Tablet:Text-l-Medium"} relative px-3 pb-3 pt-2`}
-            >
-              {menu} 11
-              {selectedTab === i && (
-                <span className="absolute bottom-[5px] left-1/2 h-[2px] w-5 -translate-x-1/2 rounded-sm bg-Primary"></span>
-              )}
-            </button>
-          ))}
-        </div>
+        <CommonTab
+          {...{
+            tabs,
+            activeTab,
+            setActiveTab,
+          }}
+        />
         <Dropdown>
           <Dropdown.Trigger>
             <Button variant={"textIconL"} className="Text-s-Medium">
@@ -60,7 +51,7 @@ export default function Activity() {
           </Dropdown.List>
         </Dropdown>
       </div>
-      {!selectedTab ? <ReviewList /> : <RatingList />}
+      {activeTab === tabs[0] ? <ReviewList /> : <RatingList />}
     </section>
   );
 }
