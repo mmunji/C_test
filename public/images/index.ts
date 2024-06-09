f7734eef1a853e2cc58d3a941d6dfaf7798a79f3
lovaoi777 <lovaoi777@naver.com>
2024-06-09 21:36:43 +0900

    Banner Image 추가

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,3 +1,7 @@
 /* eslint-disable simple-import-sort/exports */
 
 export { default as DefaultProfile } from "./detail/default_profile.png";
+export { default as DetailBanner } from "./detail/detail-banner-example.png";
+export { default as CastAndCrew } from "./detail/detail-cast-and-crew-example.png";
+export { default as DetailPoster } from "./detail/detail-poster-example.png";
+export { default as DetailTrailer } from "./detail/detail-trailer-example.png";
