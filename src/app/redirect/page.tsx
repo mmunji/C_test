a4217d5e55c7eee130784d2da2e50a63b3217b6e
Devinix00 <dpldpl123456@naver.com>
2024-06-26 00:03:27 +0900

    CIN-81: Redirect 페이지의 sessionStorage useEffect 내부에 위치

--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -8,12 +8,13 @@ import { authAPIS } from "@/services/auth/authAPIs";
 
 export default function Redirect() {
   const router = useRouter();
-  const prevPage = sessionStorage.getItem("prev-page");
   const searchParams = new URLSearchParams(window.location.search);
   const authToken = searchParams.get("authToken");
 
   useEffect(() => {
     const fetchLogin = async () => {
+      const prevPage = sessionStorage.getItem("prev-page");
+
       if (authToken) {
         const { res } = await authAPIS.authBy(authToken);
         console.log(res.headers.get("access"));
@@ -24,7 +25,7 @@ export default function Redirect() {
       }
     };
     fetchLogin();
-  }, [authToken, router, prevPage]);
+  }, [authToken, router]);
 
   return (
     <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
