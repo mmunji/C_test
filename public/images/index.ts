5e8f7a3d816988a18a798590154183e260245a7d
Devinix <121074744+Devinix00@users.noreply.github.com>
2024-07-04 14:39:55 +0900

    CIN-81: 회원가입 시 씨콩이 사진 추가

--- public/images/index.ts
+++ public/images/index.ts
@@ -7,3 +7,4 @@ export { default as DetailPoster } from "./detail/detail-poster-example.png";
 export { default as DetailTrailer } from "./detail/detail-trailer-example.png";
 export { default as FullLogo } from "./cinetalk_logo/PNG/Cinetalk_Logo_Full.png";
 export { default as ShortLogo } from "./cinetalk_logo/PNG/Cinetalk_Logo_Short.png";
+export { default as WelcomeSsikongi } from "./씨콩이/PNG/Welcome.png";
--- src/app/sign-up-complete/_components/NotMobileSignUpComplete.tsx
+++ src/app/sign-up-complete/_components/NotMobileSignUpComplete.tsx
@@ -3,6 +3,8 @@ import { useSearchParams } from "next/navigation";
 
 import Button from "@/components/buttons/Button";
 
+import { WelcomeSsikongi } from "../../../../public/images";
+
 interface NotMobileSignUpCompleteProps {
   prevPage: string;
 }
@@ -13,14 +15,16 @@ export default function NotMobileSignUpComplete({
   const nickname = useSearchParams().get("nickname");
 
   return (
-    <div className="hidden h-[100vh] w-[100vw] items-center justify-center rounded-xl bg-D1_Gray Tablet:flex Tablet:h-[520px] Tablet:w-[472px] Tablet:flex-col Tablet:gap-10 Tablet:px-14 Tablet:py-16 Laptop:h-[404px] Laptop:w-[440px] Laptop:gap-6 Laptop:px-10 Laptop:py-11 Desktop:h-[603px] Desktop:w-[622px] Desktop:gap-10 Desktop:px-[100px] Desktop:py-20">
+    <div className="hidden items-center justify-center rounded-xl bg-D1_Gray Tablet:flex Tablet:h-[558px] Tablet:w-[472px] Tablet:flex-col Tablet:gap-10 Tablet:px-14 Tablet:py-16 Laptop:h-[438px] Laptop:w-[440px] Laptop:gap-6 Laptop:px-10 Laptop:py-11 Desktop:h-[603px] Desktop:w-[650px] Desktop:gap-10 Desktop:px-[100px] Desktop:py-20">
       <Image
-        src=""
-        alt=""
-        className="w-full Tablet:h-[185px] Laptop:h-[150px] Desktop:h-[227px]"
+        src={WelcomeSsikongi}
+        alt="씨콩이"
+        className="Tablet:h-[184px] Tablet:w-[344px] Laptop:h-[150px] Laptop:w-[280px] Desktop:h-[226px] Desktop:w-[422px]"
       />
       <section className="flex flex-col items-center text-center Tablet:gap-4 Laptop:gap-3 Desktop:gap-4">
-        <h2 className="text-Primary Text-xxl-Bold">어서오세요 {nickname}님!</h2>
+        <h2 className="text-Primary Text-xxl-Bold">
+          어서오세요 <br /> {nickname}님!
+        </h2>
         <p className="font-Medium">
           이제 씨네톡에서 같이 즐거운 시간 보내요 :)
         </p>
