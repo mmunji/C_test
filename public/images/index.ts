4105910e90e206ffd9f646cc1febf4469d569d71
Devinix <121074744+Devinix00@users.noreply.github.com>
2024-07-04 22:54:03 +0900

    CIN-81: 씨콩이 디렉토리 영어로 변경

--- public/images/index.ts
+++ public/images/index.ts
@@ -7,4 +7,4 @@ export { default as DetailPoster } from "./detail/detail-poster-example.png";
 export { default as DetailTrailer } from "./detail/detail-trailer-example.png";
 export { default as FullLogo } from "./cinetalk_logo/PNG/Cinetalk_Logo_Full.png";
 export { default as ShortLogo } from "./cinetalk_logo/PNG/Cinetalk_Logo_Short.png";
-export { default as WelcomeSsikongi } from "./씨콩이/PNG/Welcome.png";
+export { default as WelcomeSsikongi } from "./ssikongi/PNG/Welcome.png";
