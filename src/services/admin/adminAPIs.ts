1f0508241a2926d5eaaa7de1f9b1e75e94ed3fae
lovaoi777 <lovaoi777@naver.com>
2024-09-11 18:33:48 +0900

    통계 Chart

---
+++ src/services/admin/adminAPIs.ts
@@ -0,0 +1,66 @@
+import { API_URL } from "@/constants/api_url";
+
+import { tokenManager } from "../auth/tokenManager";
+
+export const adminAPIs = {
+  FeedbackList: async () => {
+    const accessToken = tokenManager.getToken();
+    const res = await fetch(`${API_URL}/afeedback/feedBackList`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        access: `${accessToken}`,
+      },
+    });
+    const data = await res.json();
+    return data;
+  },
+  ReportsList: async () => {
+    const accessToken = tokenManager.getToken();
+    const res = await fetch(`${API_URL}/areports/list`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        access: `${accessToken}`,
+      },
+    });
+    const data = await res.json();
+    return data;
+  },
+  UserCount: async () => {
+    const accessToken = tokenManager.getToken();
+    const res = await fetch(`${API_URL}/amain/userCountList`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        access: `${accessToken}`,
+      },
+    });
+    const data: Users = await res.json();
+    return data;
+  },
+  ReviewCount: async () => {
+    const accessToken = tokenManager.getToken();
+    const res = await fetch(`${API_URL}/amain/reviewCountList`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        access: `${accessToken}`,
+      },
+    });
+    const data: Users = await res.json();
+    return data;
+  },
+  KeywordwCount: async () => {
+    const accessToken = tokenManager.getToken();
+    const res = await fetch(`${API_URL}/amain/keywordCountList`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        access: `${accessToken}`,
+      },
+    });
+    const data: Users = await res.json();
+    return data;
+  },
+};
