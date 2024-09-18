1b12c2a2e1c564ca2741821d6eec7d0effad809b
lovaoi777 <lovaoi777@naver.com>
2024-09-18 14:05:33 +0900

    feat : Modal 활성화

--- src/app/admin/_components/TableList.tsx
+++ src/app/admin/_components/TableList.tsx
@@ -6,6 +6,11 @@ import { ColDef, RowNode } from "ag-grid-community";
 import { AgGridReact } from "ag-grid-react";
 import dayjs from "dayjs";
 import { useState } from "react";
+
+import Modal from "@/components/modal/modal";
+
+import FeedBackModal from "./Modal/FeedbackModal";
+import ReportModal from "./Modal/ReportModal";
 interface RowData {
   id: number;
   user_email: string;
@@ -31,7 +36,7 @@ export default function TableList({ rowData, type }: TableListProps) {
       sort: "asc",
     },
     { headerName: "일시", field: "createdAt", width: 200 },
-    { headerName: "닉네임", field: "nickname", width: 200 },
+    { headerName: "닉네임", field: "user_nickName", width: 200 },
     { headerName: "카테고리", field: "category", width: 200 },
     { headerName: "신고한 리뷰", field: "content", width: 200 },
     { headerName: "신고 내용", field: "review_content", flex: 1 },
@@ -45,8 +50,25 @@ export default function TableList({ rowData, type }: TableListProps) {
 
   const [filteredData, setFilteredData] = useState<RowData[]>(rowData);
   const [searchText, setSearchText] = useState("");
-
+  const [isListModal, setisListModal] = useState(false);
+  const [isReportModal, setisReportModal] = useState(false);
+  const [ModalContent, setModalContent] = useState<{
+    user_email: string;
+    user_nickName: string;
+    content: string;
+  } | null>(null);
+  const [ReportContent, setReportContent] = useState<{
+    id: number;
+    user_nickName: string;
+    user_email: string;
+    category: string;
+    content: string;
+    review_content: string;
+    createdAt: string;
+  } | null>(null);
   // 검색 기능 구현
+
+  // const [ReportContent, setReportContent] = useState("");
   const onSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     setSearchText(value);
@@ -66,27 +88,59 @@ export default function TableList({ rowData, type }: TableListProps) {
       );
     setFilteredData(filtered);
   };
+  const setFeedBackIsModal = (params: any) => {
+    setModalContent(params.data);
+    setisListModal((prev) => !prev);
+  };
+  const setReportBackIsModal = (params: any) => {
+    setReportContent(params.data);
+    setisReportModal((prev) => !prev);
+    console.log("report");
+  };
 
   return (
-    <div className="min-h-screen w-full p-6">
-      <h1 className="mb-6 text-center text-4xl font-extrabold text-gray-800">
-        {type == "report" ? "신고" : "피드백"}
-      </h1>
-      <div className="mb-4 flex justify-center">
-        <input
-          type="text"
-          value={searchText}
-          onChange={onSearchChange}
-          placeholder="검색어 입력"
-          className="rounded-md border border-gray-300 p-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
+    <div className="flex min-h-screen w-full flex-col gap-5 p-6">
+      <div className="flex items-center justify-between p-10">
+        <h1 className="mb-6 text-center  text-4xl font-extrabold text-gray-800 ">
+          {type == "report" ? "신고" : "피드백"}
+        </h1>
+        <div className="mb-4 flex items-center justify-center gap-4">
+          <span className="">조회할 데이터 : </span>
+          <input
+            type="text"
+            value={searchText}
+            onChange={onSearchChange}
+            placeholder="검색어 입력"
+            className="rounded-md border border-gray-300 p-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
       </div>
       <div className="ag-theme-alpine w-full" style={{ height: "400px" }}>
         <AgGridReact
           rowData={filteredData}
           columnDefs={type == "report" ? ReportColumnDefs : columnDefs}
+          onCellClicked={
+            type == "report" ? setReportBackIsModal : setFeedBackIsModal
+          }
         />
       </div>
+      {isReportModal && ReportContent ? (
+        <ReportModal
+          isReportModal={isReportModal}
+          setIsModal={setReportBackIsModal}
+          ReportContent={ReportContent}
+          setisReporttModal={setisReportModal}
+        />
+      ) : null}
+
+      {isListModal && ModalContent ? (
+        <FeedBackModal
+          isListModal={isListModal}
+          setIsModal={setFeedBackIsModal}
+          ModalContent={ModalContent}
+          setisListModal={setisListModal}
+        />
+      ) : null}
     </div>
   );
 }
