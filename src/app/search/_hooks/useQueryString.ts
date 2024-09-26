2147579ddba4f740f65cc49bb6c86b91baf754d1
bisari31 <bisari31@gmail.com>
2024-09-26 22:30:28 +0900

    feat: tab 애니메이션 추가

--- src/app/search/_hooks/useQueryString.ts
+++ src/app/search/_hooks/useQueryString.ts
@@ -4,6 +4,7 @@ export default function useQueryString() {
   const searchParams = useSearchParams();
   const tab = searchParams.get("tab");
   const query = searchParams.get("query");
+  const filter = searchParams.get("sort");
 
-  return { tab: !tab || tab === "전체" ? "전체" : tab, query };
+  return { tab, query, filter };
 }
