c1e14c3aea6d91d41839dd3fc2d20cfa3839aabf
lovaoi777 <lovaoi777@naver.com>
2024-09-27 15:48:51 +0900

    fix : codeError

--- src/app/(main)/_components/SectionMoiveList/MovieTopRank/Post/BestTalkPost.tsx
+++ src/app/(main)/_components/SectionMoiveList/MovieTopRank/Post/BestTalkPost.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 
 import { StarFillSm, ThumbsUpLineMd } from "@/../public/icons";
 
-import Profile from "../../../Profile/Profile";
 interface ReviewType {
   star: number;
   content: string;
@@ -20,7 +19,14 @@ export default function BestTalkPost({
     <div className="flex  w-full items-center justify-between gap-3  rounded-xl bg-D1_Gray px-[16px] py-[8px] ">
       <div className="flex flex-row items-center justify-center Desktop:flex-col">
         <div className="flex gap-1">
-          {/* <Profile url={profileImg} /> */}
+          <div
+            className="h-[24px] w-[24px] rounded-[60px] "
+            style={{
+              backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.0) 0%, rgba(0, 0, 0, 0) 100%), url(data:image/jpeg;base64,${profileImg}`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          />
           <div className=" flex items-center Text-s-Bold">
             <Image src={StarFillSm} alt="별점" />
             <span className="Text-s-Medium">{star}</span>
