adbab614181c96bffdb1d93a8496d8e5843035b9
김범수 <121074744+Devinix00@users.noreply.github.com>
2024-10-11 02:38:00 +0900

    CIN-26: 출연, 제작진 사진 비율 조절

--- src/app/detail/_components/common/CastAndCrewSlider.tsx
+++ src/app/detail/_components/common/CastAndCrewSlider.tsx
@@ -64,7 +64,7 @@ export default function CastAndCrewSlider({
                   height={100}
                   src={profileImage}
                   alt={el.name}
-                  className="h-[112px] rounded-[8px]"
+                  className="h-[112px] rounded-[8px] object-cover"
                 />
                 <section className="flex h-[79px] flex-col px-2 py-2">
                   <p className="line-clamp-2 h-10 overflow-hidden text-ellipsis break-all leading-[150%] text-Silver Text-s-Medium">
@@ -93,7 +93,7 @@ export default function CastAndCrewSlider({
                   height={100}
                   src={profileImage}
                   alt={el.name}
-                  className="h-[112px] rounded-[8px]"
+                  className="h-[112px] rounded-[8px] object-cover"
                 />
                 <section className="flex h-[79px] flex-col px-2 py-2">
                   <p className="line-clamp-2 h-10 overflow-hidden text-ellipsis break-all leading-[150%] text-Silver Text-s-Medium">
