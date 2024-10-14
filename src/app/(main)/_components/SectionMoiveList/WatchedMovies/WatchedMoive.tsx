c3d76dbb9e3559a9b3675471d633f5cbfa9d8c8c
lovaoi777 <lovaoi777@naver.com>
2024-10-14 20:46:58 +0900

    fix : design 수정

--- src/app/(main)/_components/SectionMoiveList/WatchedMovies/WatchedMoive.tsx
+++ src/app/(main)/_components/SectionMoiveList/WatchedMovies/WatchedMoive.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import SpeechBubble from "@/components/speechBubble/SpeechBubble";
+import { tokenManager } from "@/services/auth/tokenManager";
 
 import MoviePosts from "./MoviePosts";
 
 export default function WatchedMoive() {
+  const accessToken = tokenManager.getToken();
   return (
     <div className="flex flex-col gap-5">
       <div className="flex flex-col gap-1 text-center  Laptop:flex-row Laptop:gap-5 Laptop:text-left">
@@ -12,7 +14,13 @@ export default function WatchedMoive() {
           혹시 이 영화 보셨나요?
         </h1>
         <div className="hidden  Laptop:block">
-          <SpeechBubble dir="left">별을 눌러 평가해보세요 :)</SpeechBubble>
+          {accessToken ? (
+            <SpeechBubble dir="left">별을 눌러 평가해보세요 :)</SpeechBubble>
+          ) : (
+            <SpeechBubble dir="left">
+              로그인 하고 별을 눌러 평가해보세요 :)
+            </SpeechBubble>
+          )}
         </div>
         <span className=" text-L_Gray Text-m-Regular Laptop:hidden  ">
           간편하게 영화를 평가해보세요!
