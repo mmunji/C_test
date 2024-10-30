aa79b8879b90bd954aacda1df9a53cf31b4ca1e1
김범수 <121074744+Devinix00@users.noreply.github.com>
2024-10-30 22:42:36 +0900

    CIN-26: 검색창 placeholder 변경

--- src/components/header/headerRightSection/headerSearchInputSection/RenderSearchInput.tsx
+++ src/components/header/headerRightSection/headerSearchInputSection/RenderSearchInput.tsx
@@ -51,7 +51,7 @@ function RenderSearchInput({
             onChange={(e: ChangeEvent<HTMLInputElement>) =>
               setInputValue(e.target.value)
             }
-            placeholder="‘파묘’ 궁금하지 않으세요?"
+            placeholder="어떤 영화가 궁금하신가요?"
             onFocus={() => setIsInputFocused(true)}
             onBlur={() => {
               setTimeout(() => {
