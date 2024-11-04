7b7de379caedd0959c074d3067afda3d4ce5aacc
김범수 <121074744+Devinix00@users.noreply.github.com>
2024-11-05 01:32:50 +0900

    CIN-26: 회원가입 -> 생년월일 디자인 수정

--- src/app/redirect/without-nickname/_components/SignUpBirth.tsx
+++ src/app/redirect/without-nickname/_components/SignUpBirth.tsx
@@ -1,5 +1,11 @@
 import dayjs from "dayjs";
-import React, { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
+import React, {
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+  useRef,
+  useState,
+} from "react";
 
 interface BirthValues {
   year: string;
@@ -22,6 +28,14 @@ export default function SignUpBirth({
 }: SignUpBirthProps) {
   const today = dayjs();
   const hundredYearsAgo = today.subtract(100, "year");
+  const yearRef = useRef<HTMLInputElement>(null);
+  const monthRef = useRef<HTMLInputElement>(null);
+  const dayRef = useRef<HTMLInputElement>(null);
+  const [inputsFocus, setInputsFocus] = useState({
+    year: false,
+    month: false,
+    day: false,
+  });
 
   const validateBirthday = (year: string, month: string, day: string) => {
     if (!year || !month || !day) {
@@ -29,7 +43,6 @@ export default function SignUpBirth({
       return;
     }
 
-    // month와 day를 두 자리로 패딩 처리
     const paddedMonth = month.padStart(2, "0");
     const paddedDay = day.padStart(2, "0");
 
@@ -73,9 +86,12 @@ export default function SignUpBirth({
 
   const handleBlur = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
+    if (name === "month") setInputsFocus((prev) => ({ ...prev, month: false }));
+    if (name === "day") setInputsFocus((prev) => ({ ...prev, day: false }));
 
     if (name === "month" || name === "day") {
-      const formattedValue = value.padStart(2, "0"); // 한 자리일 경우 앞에 0을 추가
+      const formattedValue = value.padStart(2, "0");
+
       setBirthValues((prev) => ({
         ...prev,
         [name]: formattedValue,
@@ -89,45 +105,52 @@ export default function SignUpBirth({
       <section className="mt-1 flex h-12 items-center gap-3">
         <section className="flex-grow-1 flex items-center gap-2">
           <input
+            ref={yearRef}
             type="text"
             name="year"
             value={birthValues.year}
+            onFocus={() => setInputsFocus((prev) => ({ ...prev, year: true }))}
+            onBlur={() => setInputsFocus((prev) => ({ ...prev, year: false }))}
             onChange={handleChange}
             maxLength={4}
             placeholder="YYYY"
-            className={`h-12 w-full rounded-xl border-[1px] ${birthError ? "border-red-500" : "border-Gray"} bg-transparent p-3 text-center outline-none Text-m-Medium placeholder:text-Gray`}
+            className={`h-12 w-full rounded-xl border-[1px] ${(birthValues.year.length === 0 || !birthError) && inputsFocus.year && "border-Tint_1"} ${birthError ? "border-Error" : "border-Gray"} bg-transparent p-3 text-center outline-none Text-m-Medium placeholder:text-Gray`}
           />
           <p className="text-Gray Text-m-Medium">년</p>
         </section>
         <section className="flex-grow-1 flex items-center gap-2">
           <input
+            ref={monthRef}
             type="text"
             name="month"
             value={birthValues.month}
+            onFocus={() => setInputsFocus((prev) => ({ ...prev, month: true }))}
             onChange={handleChange}
             onBlur={handleBlur}
             maxLength={2}
             placeholder="MM"
-            className={`h-12 w-full rounded-xl border-[1px] ${birthError ? "border-red-500" : "border-Gray"} bg-transparent p-3 text-center outline-none Text-m-Medium placeholder:text-Gray`}
+            className={`h-12 w-full rounded-xl border-[1px] ${(birthValues.month.length === 0 || !birthError) && inputsFocus.month && "border-Tint_1"} ${birthError ? "border-Error" : "border-Gray"} bg-transparent p-3 text-center outline-none Text-m-Medium placeholder:text-Gray`}
           />
           <p className="text-Gray Text-m-Medium">월</p>
         </section>
         <section className="flex-grow-1 flex items-center gap-2">
           <input
+            ref={dayRef}
             type="text"
             name="day"
             value={birthValues.day}
+            onFocus={() => setInputsFocus((prev) => ({ ...prev, day: true }))}
             onChange={handleChange}
             onBlur={handleBlur}
             maxLength={2}
             placeholder="DD"
-            className={`h-12 w-full rounded-xl border-[1px] ${birthError ? "border-red-500" : "border-Gray"} bg-transparent p-3 text-center outline-none Text-m-Medium placeholder:text-Gray`}
+            className={`h-12 w-full rounded-xl border-[1px] ${(birthValues.day.length === 0 || !birthError) && inputsFocus.day && "border-Tint_1"} ${birthError ? "border-Error" : "border-Gray"} bg-transparent p-3 text-center outline-none Text-m-Medium placeholder:text-Gray`}
           />
           <p className="text-Gray Text-m-Medium">일</p>
         </section>
       </section>
       {birthError && (
-        <p className="mt-2 text-red-500 Text-xs-Regular">
+        <p className="mt-2 text-Error Text-xs-Regular">
           올바른 생년월일을 입력하세요.
         </p>
       )}
--- src/app/redirect/without-nickname/_components/SignUpNickname.tsx
+++ src/app/redirect/without-nickname/_components/SignUpNickname.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React, { Dispatch, SetStateAction, useState } from "react";
 
 import { nicknameRegex } from "@/utils/regex";
@@ -17,6 +18,7 @@ export default function SignUpNickname({
 }: SignUpNicknameProps) {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
+    if (value.length > 10) return;
     setNickname(value);
 
     if (nicknameRegex.test(value)) {
@@ -29,15 +31,26 @@ export default function SignUpNickname({
   return (
     <section>
       <p className="text-White Text-xs-Regular">닉네임</p>
-      <input
-        type="text"
-        name="nickname"
-        value={nickname}
-        onChange={handleChange}
-        className={`mt-2 h-12 w-full rounded-xl border-[1px] ${nickError ? "border-red-500" : "border-Gray"} bg-transparent px-5 py-3 outline-none Text-m-Medium`}
-      />
+      <div className="relative mt-2">
+        <input
+          type="text"
+          name="nickname"
+          maxLength={10}
+          value={nickname}
+          onChange={handleChange}
+          className={`h-12 w-full rounded-xl border-[1px] ${nickError ? "border-Error" : "border-Gray"} bg-transparent px-5 py-3 outline-none Text-m-Medium`}
+        />
+        <div
+          className={clsx(
+            "absolute right-5 top-1/2 -translate-y-1/2 Text-m-Medium",
+            nickError ? "text-Error" : "text-Gray",
+          )}
+        >
+          {nickname.length}/10
+        </div>
+      </div>
       {nickError && (
-        <p className="mt-2 text-red-500 Text-xs-Regular">
+        <p className="mt-2 text-Error Text-xs-Regular">
           닉네임은 1자 이상 10자 이하의 영문, 숫자, 한글만 가능합니다.
         </p>
       )}
