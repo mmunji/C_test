6417ea757144492e2767fd3c86cb03e5188aa3c9
김범수 <121074744+Devinix00@users.noreply.github.com>
2024-11-08 01:32:13 +0900

    CIN-26: dirveTalkText 수정

--- src/app/detail/_components/keywordAndTalkAndGallery/talk/rating/Rating.tsx
+++ src/app/detail/_components/keywordAndTalkAndGallery/talk/rating/Rating.tsx
@@ -6,8 +6,6 @@ import Modal from "@/components/modal/modal";
 import RatingStar from "@/components/rating/RatingStar";
 import SpeechBubble from "@/components/speechBubble/SpeechBubble";
 import useHandleClickAuthButton from "@/hooks/useHandleClickAuthButtons";
-import useNeedLogin from "@/hooks/useNeedLogin";
-import { useGetMyTalk } from "@/services/talk/talkQueries";
 import useLoggedInStore from "@/stores/useLoggedIn";
 
 import DriveCommentText from "./DriveCommentText";
@@ -39,7 +37,7 @@ export default function Rating({
     setIsOpen,
     showTalkForm,
     setShowTalkForm,
-  } = useRating({ initialValue: myTalk ? myTalk.star : 0 });
+  } = useRating({ initialValue: myTalk ? myTalk?.star : 0, myTalk });
   const { handleClickAuthButton } = useHandleClickAuthButton();
   const genreList = movieDetailData.genreDTOList.map((el) => el.id);
   const [isLoading, setIsLoading] = useState(false);
@@ -67,29 +65,47 @@ export default function Rating({
       )}
 
       <div className="mx-auto mb-6 flex cursor-pointer Laptop:mb-0">
-        {myTalk && !showTalkForm ? (
-          <FixedRating star={myTalk?.star || 0} />
-        ) : (
-          [...Array(5)].map((_, i) => (
-            <RatingStar
-              key={i}
-              {...{
-                type: "detail",
-                movienm: movieDetailData.title,
-                index: i,
-                ratingValue,
-                setRatingValue,
-                clickedValue,
-                setClickedValue,
-                ratingSize: "Xl",
-                readyToRating,
-                StarReview: showTalkForm ? false : true,
-                movieId: movieId,
-                genreList: genreList,
-              }}
-            />
-          ))
-        )}
+        {myTalk
+          ? [...Array(5)].map((_, i) => (
+              <RatingStar
+                key={i}
+                {...{
+                  myTalk: myTalk,
+                  type: "detail-edit",
+                  movienm: movieDetailData.title,
+                  index: i,
+                  ratingValue,
+                  setRatingValue,
+                  clickedValue,
+                  setClickedValue,
+                  ratingSize: "Xl",
+                  readyToRating,
+                  StarReview: showTalkForm ? false : true,
+                  movieId: movieId,
+                  genreList: genreList,
+                }}
+              />
+            ))
+          : [...Array(5)].map((_, i) => (
+              <RatingStar
+                key={i}
+                {...{
+                  myTalk: myTalk,
+                  type: "detail",
+                  movienm: movieDetailData.title,
+                  index: i,
+                  ratingValue,
+                  setRatingValue,
+                  clickedValue,
+                  setClickedValue,
+                  ratingSize: "Xl",
+                  readyToRating,
+                  StarReview: showTalkForm ? false : true,
+                  movieId: movieId,
+                  genreList: genreList,
+                }}
+              />
+            ))}
       </div>
 
       {isLoading && loggedIn && (
--- src/app/detail/_hooks/useRating.ts
+++ src/app/detail/_hooks/useRating.ts
@@ -4,15 +4,26 @@ import useNeedLogin from "@/hooks/useNeedLogin";
 
 interface UseRatingProps {
   initialValue?: number;
+  myTalk?: MyTalk;
 }
 
-export default function useRating({ initialValue = 0 }: UseRatingProps) {
+export default function useRating({
+  initialValue = 0,
+  myTalk,
+}: UseRatingProps) {
   const [showTalkForm, setShowTalkForm] = useState(false);
   const [ratingValue, setRatingValue] = useState<number>(initialValue);
   const [clickedValue, setClickedValue] = useState(false);
   const [driveTalkText, setDriveTalkText] = useState("");
   const [readyToRating, setReadyToRating] = useState(false);
   const { isOpen, setIsOpen, handleNeedLogin } = useNeedLogin();
+
+  useEffect(() => {
+    if (myTalk) {
+      setRatingValue(myTalk.star);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [myTalk]);
 
   useEffect(() => {
     if (clickedValue) {
@@ -39,12 +50,29 @@ export default function useRating({ initialValue = 0 }: UseRatingProps) {
       setDriveTalkText("최고였군요! 더 자세한 얘기도 해주세요 :)");
     }
   };
-  console.log("driveTalkText", driveTalkText);
 
   useEffect(() => {
-    if ((clickedValue || ratingValue) && !showTalkForm) handleDriveTalk();
+    if (myTalk) {
+      if (ratingValue === 0.5 || ratingValue === 1 || ratingValue === 1.5) {
+        setDriveTalkText("별로였군요ㅠㅠ 더 자세한 얘기도 해주세요 :)");
+      } else if (ratingValue === 2 || ratingValue === 2.5) {
+        setDriveTalkText("아쉬웠군요. 더 자세한 얘기도 해주세요 :)");
+      } else if (ratingValue === 3 || ratingValue === 3.5) {
+        setDriveTalkText("보통이었군요! 더 자세한 얘기도 해주세요 :)");
+      } else if (ratingValue === 4 || ratingValue === 4.5) {
+        setDriveTalkText("좋았군요! 더 자세한 얘기도 해주세요 :)");
+      } else if (ratingValue === 5) {
+        setDriveTalkText("최고였군요! 더 자세한 얘기도 해주세요 :)");
+      }
+    }
+  }, [myTalk, ratingValue]);
+
+  useEffect(() => {
+    if ((clickedValue || ratingValue || myTalk) && !showTalkForm) {
+      handleDriveTalk();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [clickedValue, showTalkForm]);
+  }, [clickedValue, showTalkForm, myTalk]);
 
   useEffect(() => {
     if (showTalkForm) setDriveTalkText("");
