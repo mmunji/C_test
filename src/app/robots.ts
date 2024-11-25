8f44f8fc73cf49aa40e53bde45a2a07ca148bd27
bisari31 <bisari31@gmail.com>
2024-11-25 10:38:51 +0900

    feat: create robots.ts

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${process.env.NEXT_PUBLIC_CLIENT_URL}/sitemap.xml`,
+  };
+}
