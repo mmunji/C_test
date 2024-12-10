54ad33452d0183367c2963b4513035a7a4fa32a7
lovaoi777 <lovaoi777@naver.com>
2024-12-10 13:28:30 +0900

    fix : 스켈레톤 수정

--- src/app/(main)/_components/MainSkeleton/PostCardSkeleton.tsx
+++ src/app/(main)/_components/MainSkeleton/PostCardSkeleton.tsx
@@ -4,7 +4,7 @@ interface PostCardType {
 export default function PostCardSkeleton({ color }: PostCardType) {
   return (
     <div
-      className={`h-[156px] w-full animate-pulse  cursor-pointer  overflow-hidden rounded-xl ${color == "D3" ? "bg-D3_Gray" : " bg-D2_Gray "} Tablet:h-[240px] Laptop:h-[260px]   Desktop:h-[360px]`}
+      className={` h-[230px] w-[156px] animate-pulse cursor-pointer  overflow-hidden  rounded-xl Tablet:w-full ${color == "D3" ? "bg-D3_Gray" : " bg-D2_Gray "} Tablet:h-[240px] Laptop:h-[260px]   Desktop:h-[360px]`}
     ></div>
   );
 }
--- src/app/(main)/_components/MainSkeleton/WatchedMoive/TabletWatched.tsx
+++ src/app/(main)/_components/MainSkeleton/WatchedMoive/TabletWatched.tsx
@@ -1,13 +1,16 @@
 export default function TabletWatched() {
   return (
-    <div className="mx-auto hidden w-[557px] animate-pulse bg-D1_Gray Tablet:flex  ">
-      <div className="h-[280px]   w-[200px] animate-pulse rounded-xl bg-D2_Gray " />
-      <div className="flex w-[357px] flex-col items-center justify-center gap-8">
-        <div className="flex  flex-col gap-2 ">
-          <div className="h-7   w-[160px] animate-pulse rounded-xl bg-D2_Gray " />
-          <div className="mx-auto   h-7 w-20 animate-pulse rounded-xl bg-D2_Gray " />
+    <div className="flex flex-col gap-5">
+      <div className="mx-auto hidden h-7  w-[200px] animate-pulse rounded-lg bg-D2_Gray Tablet:block" />
+      <div className="mx-auto hidden w-[557px] animate-pulse bg-D1_Gray Tablet:flex  ">
+        <div className="h-[280px]   w-[200px] animate-pulse rounded-xl bg-D2_Gray " />
+        <div className="flex w-[357px] flex-col items-center justify-center gap-8">
+          <div className="flex  flex-col gap-2 ">
+            <div className="h-7   w-[160px] animate-pulse rounded-xl bg-D2_Gray " />
+            <div className="mx-auto   h-7 w-20 animate-pulse rounded-xl bg-D2_Gray " />
+          </div>
+          <div className="h-[52px]   w-[200px] animate-pulse rounded-lg bg-D2_Gray " />
         </div>
-        <div className="h-[52px]   w-[200px] animate-pulse rounded-lg bg-D2_Gray " />
       </div>
     </div>
   );
