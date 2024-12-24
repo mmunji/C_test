f81f3e3dd154a7de88f9321fd404b4ec767ef9ad
bisari31 <bisari31@gmail.com>
2024-12-24 22:24:07 +0900

    refactor: subset폰트 변경

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,8 +1,18 @@
 import localFont from "next/font/local";
 
 export const pretendard = localFont({
-  src: "../../public/fonts/PretendardVariable.woff2",
+  src: [
+    { path: "../../public/fonts/Pretendard-Bold.subset.woff2", weight: "700" },
+    {
+      path: "../../public/fonts/Pretendard-Medium.subset.woff2",
+      weight: "500",
+    },
+    {
+      path: "../../public/fonts/Pretendard-Regular.subset.woff2",
+      weight: "400",
+    },
+  ],
+  // src: "../../public/fonts/PretendardVariable.woff2",
   display: "swap",
-  weight: "45 920",
   variable: "--font-pretendard",
 });
