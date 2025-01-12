f230d04d0695089393831d0b35fb4b9db89569a3
bisari31 <bisari31@gmail.com>
2025-01-13 00:20:37 +0900

    refactor: 마이페이지 리팩토링
    - snsLogin 컴포넌트 리팩토링

--- src/app/my/account/_components/SnsLogin.tsx
+++ src/app/my/account/_components/SnsLogin.tsx
@@ -7,21 +7,20 @@ import { Kakao, Naver } from "../../../../../public/icons";
 
 export default async function SnsLogin() {
   const user = await myAPIs.getUser();
-  const oauthProvider = user.provider === "kakao" ? "kakao" : "naver";
   return (
     <div className="flex items-center gap-3 rounded-xl bg-D1_Gray px-4 py-2 Text-s-Medium Tablet:px-8 Tablet:py-4 Tablet:Text-m-Medium">
       <div className="flex flex-1 items-center gap-4">
         <div
-          className={`my-1 flex h-8 w-8 items-center justify-center rounded-lg Tablet:my-0 Tablet:h-10 Tablet:w-10 ${oauthProvider === "kakao" ? "bg-Kakako" : "bg-Naver"}`}
+          className={`my-1 flex h-8 w-8 items-center justify-center rounded-lg Tablet:my-0 Tablet:h-10 Tablet:w-10 ${user.provider === "kakao" ? "bg-Kakako" : "bg-Naver"}`}
         >
           <Image
             unoptimized
-            alt={oauthProvider}
-            src={oauthProvider === "kakao" ? Kakao : Naver}
+            alt={user.provider}
+            src={user.provider === "kakao" ? Kakao : Naver}
           />
         </div>
         <span className="">
-          {oauthProvider === "kakao" ? "카카오" : "네이버"} 계정 연동 중
+          {user.provider === "kakao" ? "카카오" : "네이버"} 계정 연동 중
         </span>
       </div>
       <LogoutButton />
